25900d8658ef3621c536f0ae4cf2ac2af81745ca
Sashmita <sashmita0209@gmail.com>
2025-04-13 21:37:18 +0530

    cloudinary and file update config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    
+// Upload an file
+const uploadFile = async (filepath) => {
+    try {
+        if (!filepath) return null;
+        const uploadResult = await cloudinary.uploader
+            .upload(filepath, {
+                resource_type: auto
+            });
+        console.log("URL of Uplaoded file : ", uploadResult.url);
+        return uploadResult;
+    }
+    catch (e) {
+        fs.unlinkSync(filepath);//removing file path from local
+        console.log(e)
+        return null;
+    }
+    
+}
